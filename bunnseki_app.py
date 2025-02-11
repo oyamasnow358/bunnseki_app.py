7a42183684a8b077df2739b6a7d988835f3e77f2
oyamasnow358 <oyamayuuji596308@gmail.com>
2025-02-11 14:24:47 +0900

    QQ

--- bunnseki_app.py
+++ bunnseki_app.py
@@ -205,7 +205,8 @@ st.markdown("---")  # 区切り線
 st.markdown("関連Webアプリに移動する")
 st.markdown("[自立活動指導支援内容](https://aspecialeducationapp-6iuvpdfjbflp4wyvykmzey.streamlit.app/)")
 st.markdown("[発達段階能力チャート作成](https://specialexcel2apppy-bo6jrng9gyqw5dmfcgwbl5.streamlit.app//)")
-st.markdown("---")  # 区切り線   
+st.markdown("---")  # 区切り線  
+st.markdown("統計学分析ツール") 
 st.markdown("[多変量回帰分析](https://kaikiapp-tjtcczfvlg2pyhd9bjxwom.streamlit.app/)")
 st.markdown("[応用行動分析](https://abaapppy-k7um2qki5kggexf8qkfxjc.streamlit.app/)")
 st.markdown("[機能的行動評価分析](https://kinoukoudou-ptfpnkq3uqgaorabcyzgf2.streamlit.app/)")
